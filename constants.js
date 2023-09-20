f570e7163f12112ea7b5305ebfe7ae81b558866c
Katie K <katiekucsera@gmail.com>
2023-09-20 15:31:02 -0400

    Store constants as variables in separate file, export for use

---
+++ constants.js
@@ -0,0 +1,16 @@
+const IP = 'localhost';
+const PORT = 50541;
+
+const UP_KEY = 'Move: up';
+
+const LEFT_KEY = 'Move: left';
+
+const DOWN_KEY = 'Move: down';
+
+const RIGHT_KEY = 'Move: right';
+
+const TAUNT_KEY = 'Say: Do u even snek, bro?';
+
+const EAT_KEY = 'Say: Omnomnom!';
+
+module.exports = {IP, PORT, UP_KEY, LEFT_KEY, DOWN_KEY, RIGHT_KEY, TAUNT_KEY, EAT_KEY};
