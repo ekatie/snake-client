2386d7de64ef80e46692b958d41badac36356b7e
Katie K <katiekucsera@gmail.com>
2023-09-20 14:24:24 -0400

    Add handling of user input keys to move snake

--- input.js
+++ input.js
@@ -1,4 +1,9 @@
-const setupInput = function() {
+const client = require('./client');
+
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,8 +16,18 @@ const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
   }
+  else if (key === 'w') {
+    connection.write('Move: up');
+  }
+  else if (key === 'a') {
+    connection.write('Move: left');
+  }
+  else if (key === 's') {
+    connection.write('Move: down');
+  }
+  else if (key === 'd') {
+    connection.write('Move: right');
+  }
 };
-
-setupInput();
 
 module.exports = {setupInput};
