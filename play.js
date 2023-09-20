9108c7c8154ac0cbf76df2905821c7b72b322f60
Katie K <katiekucsera@gmail.com>
2023-09-20 13:54:01 -0400

    Move back console log and call of connect function from client.js

--- play.js
+++ play.js
@@ -1,16 +1,5 @@
 const {connect} = require('./client');
+const {setupInput} = require('./input');
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput = (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-  return stdin;
-};
-
-setupInput();
+console.log("Connecting...");
+connect();
