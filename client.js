df44f868d8cd5bc115dd4fc1ccedde1bc8aecf8c
Katie K <katiekucsera@gmail.com>
2023-09-23 19:42:18 -0400

    Add commented out code for new played joined message from server

--- client.js
+++ client.js
@@ -14,6 +14,32 @@ const connect = function() {
     conn.write('Name: KTK');
   });
 
+  conn.on("data", () => {
+    console.log('you ded cuz you idled');
+  });
+
+  return conn;
+};
+
+module.exports = {connect};
+
+/* Code for use with amended server game.js file
+const net = require('net');
+const {IP, PORT} = require('./constants');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT,
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to the game server");
+    conn.write('Name: KTK');
+  });
+
   conn.on("data", (data) => {
     // Display "you ded cuz you idled" when game times out, or 
     // "A new player has joined the game!" when another player joins
@@ -24,3 +50,4 @@ const connect = function() {
 };
 
 module.exports = {connect};
+*/
